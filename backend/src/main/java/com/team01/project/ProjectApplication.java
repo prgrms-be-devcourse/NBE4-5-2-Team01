54501728f1ec7728ab4927b261b7fe9b19fbbc81
sameom1048 <samvision99@naver.com>
2025-03-04 14:26:05 +0900

    :sparkles: Feat: 스케줄러 기능 구현 위해 `@EnableScheduling` 추가

--- backend/src/main/java/com/team01/project/ProjectApplication.java
+++ backend/src/main/java/com/team01/project/ProjectApplication.java
@@ -3,13 +3,15 @@ package com.team01.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class ProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProjectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
 
 }
