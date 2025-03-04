e0316ff19fd5f6e8c9f53524a91ee121b48a40b4
shinwoos <kmnj2100@gmail.com>
2025-03-04 15:12:35 +0900

    feat: userDto 추가

--- backend/src/main/java/com/team01/project/domain/user/controller/UserController.java
+++ backend/src/main/java/com/team01/project/domain/user/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.team01.project.domain.user.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team01.project.domain.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+	private final UserService userService;
 }
--- backend/src/main/java/com/team01/project/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/team01/project/domain/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.team01.project.domain.user.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.team01.project.domain.user.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
 }
