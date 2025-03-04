899aeac6c8bf3160364de8d0e97cb84b238de7d8
jxxngin <wwd0203@daum.net>
2025-03-04 14:12:11 +0900

    Feat: 트랙 API 응답 DTO 클래스 구현

---
+++ backend/src/main/java/com/team01/project/domain/music/dto/SpotifyTrackResponse.java
@@ -0,0 +1,43 @@
+package com.team01.project.domain.music.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SpotifyTrackResponse {
+	private String id;
+	private String name;
+
+	@JsonProperty("artists")
+	private List<Artist> artists;
+
+	private Album album;
+
+	@Getter
+	@Setter
+	public static class Album {
+		private String name;
+		private String release_date;
+
+		@JsonProperty("images")
+		private List<Image> images;
+	}
+
+	@Getter
+	@Setter
+	public static class Artist {
+		private String id;
+		private String name;
+	}
+
+	@Getter
+	@Setter
+	public static class Image {
+		private String url;
+	}
+}
