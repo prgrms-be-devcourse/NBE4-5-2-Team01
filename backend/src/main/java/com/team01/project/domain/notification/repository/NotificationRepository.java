3faf382b6bd55c9f49b77582992f5f128c5476fc
sameom1048 <samvision99@naver.com>
2025-03-05 18:00:07 +0900

    :recycle: Refactor: @Repository 수정

--- backend/src/main/java/com/team01/project/domain/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/team01/project/domain/notification/repository/NotificationRepository.java
@@ -3,12 +3,10 @@ package com.team01.project.domain.notification.repository;
 import com.team01.project.domain.notification.entity.Notification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalTime;
 import java.util.List;
 
-@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 	List<Notification> findByUserId(Long userId);
 
