b7e553bfe5bd0d39946e503eefc27a5bc6977220
jxxngin <wwd0203@daum.net>
2025-03-05 15:00:25 +0900

    Refactor: Music 엔티티 Setter 제거 및 생성자 접근 제한

--- backend/src/main/java/com/team01/project/domain/music/entity/Music.java
+++ backend/src/main/java/com/team01/project/domain/music/entity/Music.java
@@ -5,17 +5,16 @@ import java.time.LocalDate;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Setter
 @Builder
 public class Music {
 
@@ -37,5 +36,4 @@ public class Music {
 
 	@Column(name = "genre")
 	private String genre;
-
 }
