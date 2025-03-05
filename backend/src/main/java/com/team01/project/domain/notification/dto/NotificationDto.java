45b19beab9735e2f6c01ee2f8b9376d3e6d5cd91
sameom1048 <samvision99@naver.com>
2025-03-05 18:21:48 +0900

    :recycle: Refactor: 기존 컨트롤러에 있던 ReqBody -> Dto로 수정 및 기존 Dto도 record로 리팩토링

--- backend/src/main/java/com/team01/project/domain/notification/dto/NotificationDto.java
+++ backend/src/main/java/com/team01/project/domain/notification/dto/NotificationDto.java
@@ -1,24 +1,23 @@
 package com.team01.project.domain.notification.dto;
 
 import com.team01.project.domain.notification.entity.Notification;
-import lombok.Getter;
 
 import java.time.LocalTime;
 
-@Getter
-public class NotificationDto {
-	Long id;
-	Long userId;
-	String message;
-	LocalTime notificationTime;
-	boolean isRead;
-
+public record NotificationDto(
+		Long id,
+		Long userId,
+		String message,
+		LocalTime notificationTime,
+		boolean isRead
+) {
 	public NotificationDto(Notification notification) {
-		this.id = notification.getId();
-		this.userId = notification.getUser().getId();
-		this.message = notification.getMessage();
-		this.notificationTime = notification.getNotificationTime();
-		this.isRead = notification.isRead();
-
+		this(
+				notification.getId(),
+				notification.getUser().getId(),
+				notification.getMessage(),
+				notification.getNotificationTime(),
+				notification.isRead()
+		);
 	}
 }
