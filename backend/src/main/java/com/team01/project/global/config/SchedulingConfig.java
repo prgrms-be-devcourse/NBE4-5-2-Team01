d5d7c0cfdf6d1fd2470ad0c8e0e9dac7cd6128f1
sameom1048 <samvision99@naver.com>
2025-03-06 12:10:03 +0900

    :recycle: Refactor: `@EnableJpaAuditing`, `@EnableScheduling` Config 파일로 따로 생성

---
+++ backend/src/main/java/com/team01/project/global/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.team01.project.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
