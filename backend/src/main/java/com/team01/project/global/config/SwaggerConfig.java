67b219bcddb00114813f0a85e532d8b2cb6a2806
shinwoos <kmnj2100@gmail.com>
2025-03-17 12:39:21 +0900

    feat. 린트수정

--- backend/src/main/java/com/team01/project/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/team01/project/global/config/SwaggerConfig.java
@@ -3,9 +3,9 @@ package com.team01.project.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
