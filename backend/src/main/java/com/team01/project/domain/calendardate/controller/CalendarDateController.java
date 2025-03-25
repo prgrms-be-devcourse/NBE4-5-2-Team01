ae282f001fde0f67abd033fe5c61175644a1f48c
jisukimtech <jisukim.tech@gmail.com>
2025-03-25 17:27:54 +0900

    Refactor: 메모 기록 수정 API HTTP 메서드 변경
    - 새로운 자원을 생성하는 것이 아니므로 기존의 POST 메서드는 적절하지 않다고 판단
    - 특정 캘린더 아이디에 대한 메모 칼럼 값을 갱신하므로 PATCH 메서드로 변경
    - 주석 및 Swagger 문서 업데이트

--- backend/src/main/java/com/team01/project/domain/calendardate/controller/CalendarDateController.java
+++ backend/src/main/java/com/team01/project/domain/calendardate/controller/CalendarDateController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -135,13 +136,13 @@ public class CalendarDateController {
 	}
 
 	/**
-	 * 메모 작성
+	 * 메모 기록 수정
 	 * @param calendarDateId 캘린더 아이디
 	 * @param request 새로운 메모
 	 * @param loggedInUser 현재 인증된 유저
 	 */
-	@Operation(summary = "작성된 메모 수정 api", description = "현재 로그인 하고 있는 유저의 메모 작성 수정할때만 사용")
-	@PostMapping("/{calendar-date-id}/memo")
+	@Operation(summary = "메모 기록 수정 api", description = "현재 인증된 유저의 특정 캘린더 날짜에 대한 메모 기록 수정")
+	@PatchMapping("/{calendar-date-id}/memo")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void writeMemoToCalendarDate(@PathVariable(name = "calendar-date-id") Long calendarDateId,
 		@RequestBody CalendarDateMemoSaveRequest request, @AuthenticationPrincipal OAuth2User loggedInUser) {
