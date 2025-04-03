bc6d67622b213017b99b60aba8bca7ffc481a79b
jisukimtech <jisukim.tech@gmail.com>
2025-04-04 03:22:15 +0900

    Feat(PermissionService): 캘린더 권한 확인 기능 통합

--- backend/src/main/java/com/team01/project/global/permission/PermissionService.java
+++ backend/src/main/java/com/team01/project/global/permission/PermissionService.java
@@ -2,10 +2,12 @@ package com.team01.project.global.permission;
 
 import static com.team01.project.domain.follow.entity.type.Status.*;
 import static com.team01.project.domain.user.entity.CalendarVisibility.*;
+import static com.team01.project.global.permission.CalendarPermission.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.team01.project.domain.follow.entity.type.Status;
 import com.team01.project.domain.follow.repository.FollowRepository;
 import com.team01.project.domain.user.entity.User;
 
@@ -19,12 +21,30 @@ public class PermissionService {
 	private final FollowRepository followRepository;
 
 	/**
+	 * 캘린더 권한 확인
+	 * @param calendarOwner 캘린더 소유자
+	 * @param loggedInUser 현재 인증된 유저
+	 * @return 캘린더 권한
+	 */
+	public CalendarPermission checkPermission(User calendarOwner, User loggedInUser) {
+		if (hasEditPermission(calendarOwner, loggedInUser)) {
+			return EDIT;
+		}
+
+		if (hasViewPermission(calendarOwner, loggedInUser)) {
+			return VIEW;
+		}
+
+		return CalendarPermission.NONE;
+	}
+
+	/**
 	 * 캘린더 조회 가능 여부 확인
 	 * @param calendarOwner 캘린더 소유자
 	 * @param loggedInUser 현재 인증된 유저
 	 * @return 캘린더 조회 가능 여부
 	 */
-	public boolean hasViewPermission(User calendarOwner, User loggedInUser) {
+	private boolean hasViewPermission(User calendarOwner, User loggedInUser) {
 
 		// 캘린더가 전체 공개로 설정돼있는 경우 조회 가능
 		if (isPublicCalendar(calendarOwner)) {
@@ -52,7 +72,7 @@ public class PermissionService {
 	 * @param loggedInUser 현재 인증된 유저
 	 * @return 캘린더 수정 가능 여부
 	 */
-	public boolean hasEditPermission(User calendarOwner, User loggedInUser) {
+	private boolean hasEditPermission(User calendarOwner, User loggedInUser) {
 		return isCalendarOwner(calendarOwner, loggedInUser);
 	}
 
@@ -65,7 +85,7 @@ public class PermissionService {
 	}
 
 	private boolean isFollower(User fromUser, User toUser) {
-		return ACCEPT == followRepository.findStatusByToUserAndFromUser(toUser, fromUser).orElse(NONE);
+		return ACCEPT == followRepository.findStatusByToUserAndFromUser(toUser, fromUser).orElse(Status.NONE);
 
 	}
 
